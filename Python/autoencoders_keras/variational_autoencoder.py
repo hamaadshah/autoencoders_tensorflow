e923b41aa5782fe6c7b1ce8ac3c7031e2395867f
hamaadshah <hamaadshah@hotmail.com>
2018-10-17 14:29:41 +0100

    Fixed VAE.

--- Python/autoencoders_keras/variational_autoencoder.py
+++ Python/autoencoders_keras/variational_autoencoder.py
@@ -170,19 +170,6 @@ class VariationalAutoencoder(BaseEstimator,
     def vae_loss(self,
                  y_true,
                  y_pred):
-        recon = self.n_feat * keras.metrics.mean_squared_error(y_true=y_true,
-                                                               y_pred=y_pred)
-        
-        # For 2D axis 0 means columns wise, i.e., across all rows sum up all columns per row.
-        # For 2D axis 1 means row wise, i.e., across all columns sum up all rows per column.
-        # For 2D axis -1 means row wise, i.e., across all columns sum up all rows per column.
-        kl = -0.5 * keras.backend.sum(x=1.0 + self.log_sigma - keras.backend.exp(self.log_sigma) - keras.backend.square(self.mu),
-                                      axis=1)
-
-        # For 2D axis 0 means columns wise, i.e., across all rows average up all columns per row.
-        # For 2D axis 1 means row wise, i.e., across all columns average up all rows per column.
-        # For 2D axis -1 means row wise, i.e., across all columns average up all rows per column.
-        out = recon + keras.backend.mean(x=kl,
-                                         axis=0)
-            
-        return out
+        recon = keras.backend.sum(x=keras.backend.square(y_pred - y_true))
+        kl = -0.5 * keras.backend.sum(x=1.0 + self.log_sigma - keras.backend.exp(self.log_sigma) - keras.backend.square(self.mu))
+        return recon + kl
